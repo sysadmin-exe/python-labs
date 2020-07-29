5e66e4b149f24f244ab911d733add6fc02b7a8f1
Victor Efedi <victorefedi@gmail.com>
2020-07-29 23:25:59 +0100

    function

---
+++ function.py
@@ -0,0 +1,10 @@
+def addition(int1,int2):
+    print(int1 + int2)
+
+addition(12,13)
+
+#return helps to pass the output of a function to a variable
+def returnAddition(int3, int4):
+    return (int3 + int4)
+sum = returnAddition(23,45)
+print(sum)
