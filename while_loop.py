e24e3bada24ab2dbc51ad1a7bcfd2baf9aba5038
Victor Efedi <victorefedi@gmail.com>
2020-08-06 23:17:47 +0100

    while loop

---
+++ while_loop.py
@@ -0,0 +1,19 @@
+i = 0
+while i < 7:
+    print("number", i)
+    i += 1
+
+
+a = 0
+while a < 7:
+    if a == 5:
+        break
+    print("number", a)
+    a += 1
+
+i = 0
+while i < 7:
+    print("number", i)
+    i += 1
+else:
+    print("this number is greater than 6")
