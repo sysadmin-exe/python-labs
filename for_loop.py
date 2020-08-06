879f276da797023e0c349601a35bf2ffad6ecbbf
Victor Efedi <victorefedi@gmail.com>
2020-08-06 21:50:01 +0100

    for loop

---
+++ for_loop.py
@@ -0,0 +1,9 @@
+#command to perform the same command more than once
+for i in range(0,4):
+    print("here we go again")
+
+#nested loop
+for a in range(4):
+    for b in range(2):
+        print("na me me dey inside")
+    print("I am outside")
