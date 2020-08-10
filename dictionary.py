044c19760b8f6404eece882c4c62d59ca3b93bac
Victor Efedi <victorefedi@gmail.com>
2020-08-11 00:09:54 +0100

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -5,11 +5,12 @@ user1 = {
     "profession": "engineer",
     "country": "Nigeria"
 }
-
+#uing get to get the value of a key in a dictionary 
 user1_country = user1.get("country")
 user1_fname = user1.get("fname")
 print(user1_fname + " is from " + user1_country)
 
+#using keys to check if a dictionary key is available
 if "country" in user1.keys():
     print("user country available")
 else:
